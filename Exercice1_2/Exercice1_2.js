21bc51061956b269ec9631021e6d506dbe31eac1
MARTINEZ Kévin <msi03@laposte.net>
2016-12-09 20:43:03 +0100

    1.4

--- Exercice1_2/Exercice1_2.js
+++ Exercice1_2/Exercice1_2.js
@@ -1,2 +1,3 @@
-var prenom = prompt("Veuillez saisir votre prenom");
-alert("Bonjour "+prenom);
+var age = prompt("Veuillez saisir votre age");
+age = parseInt(age);
+age > 18 ? alert("Vous etes majeur") : alert("vous etes mineur");
