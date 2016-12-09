530b8e7dc2ca551367cea5364d41dd9a3d85be3a
MARTINEZ Kévin <msi03@laposte.net>
2016-12-09 20:49:36 +0100

    1.6

--- Exercice1_2/Exercice1_2.js
+++ Exercice1_2/Exercice1_2.js
@@ -1,5 +1,5 @@
-var nom = "Gizzmo";
-var prenom = "Bob";
-var ville = "Clermont-Ferrand";
+var nom = prompt("Quel est votre nom ?");
+var prenom = prompt("Quel est votre prenom ?");
+var ville = prompt("Dans quelle ville habitez vous ?");
 
 alert(" nom: "+nom+"\n prenom: "+prenom+"\n ville: "+ville);
